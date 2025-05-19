ce292f19d5979feabaad76e0c58adf5777ba9780
elodiier <elodier_@outlook.fr>
2025-05-19 16:13:01 +0200

    modifpopup

--- script3.js
+++ script3.js
@@ -1,26 +1,18 @@
 const btnPopup = document.querySelector('.btnLogin-popup');
-const cover_box = document.querySelector('.cover-box');
+const coverBox = document.querySelector('.cover-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const iconClose = document.querySelector('.icon-close');
 
-function activateCoverBox(){
-    cover_box.classList.add('active');
+function openPopup(){
+    coverBox.classList.add('active');
 }
 
-function deactivateCoverBox(){
-    cover_box.classList.remove('active');
+function closePopup() {
+    coverBox.classList.remove('active');
 }
 
-function activatePopup(){
-    cover_box.classList.add('active-popup');
-}
-
-function deactivateCoverPopup(){
-    cover_box.classList.remove('active-popup');
-}
-
-registerLink.addEventListener('click', activateCoverBox);
-loginLink.addEventListener('click', deactivateCoverBox);
-btnPopup.addEventListener('click', activatePopup);
-iconClose.addEventListener('click', deactivateCoverPopup);
+if (registerLink) registerLink.addEventListener('click', openPopup);
+if (loginLink) loginLink.addEventListener('click', closePopup);
+if (btnPopup) btnPopup.addEventListener('click', openPopup);
+if (iconClose) iconClose.addEventListener('click', closePopup);
