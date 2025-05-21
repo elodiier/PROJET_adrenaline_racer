13f303d9892db53b676159332457a16815224635
elodiier <elodier_@outlook.fr>
2025-05-21 17:03:04 +0200

    OrganisationFinale

---
+++ ContactScript.js
@@ -0,0 +1,27 @@
+const btnPopup = document.querySelector('.btnLogin-popup');
+const coverBox = document.querySelector('.cover-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const iconClose = document.querySelector('.icon-close');
+
+function activateCoverBox(){
+    coverBox.classList.add('active');
+}
+
+function deactivateCoverBox() {
+    coverBox.classList.remove('active');
+    coverBox.classList.remove('active-popup');
+}
+
+function activatePopup(){
+    coverBox.classList.add('active-popup');
+}
+
+function deactivateCoverPopup(){
+    coverBox.classList.remove('active-popup');
+}
+
+btnPopup.addEventListener('click', activateCoverBox);
+loginLink.addEventListener('click', deactivateCoverBox);
+registerLink.addEventListener('click', activatePopup);
+iconClose.addEventListener('click', deactivateCoverBox);
