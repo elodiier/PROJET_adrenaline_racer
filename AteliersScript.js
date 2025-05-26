d4f6617a14558fadc2e18abe5f899961fef80a4f
elodiier <elodier_@outlook.fr>
2025-05-26 17:11:31 +0200

    dernieremodifs

---
+++ AteliersScript.js
@@ -0,0 +1,21 @@
+    function openPopup(popupId) {
+        document.getElementById(popupId).style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+    }
+    function closePopup() {
+        const popups = document.querySelectorAll('.popup');
+        popups.forEach(popup => {
+            popup.style.display = 'none';
+        });
+        document.getElementById('overlay').style.display = 'none';
+    }
+
+    const burgerMenuButton = document.querySelector('.burger-menu-button')
+    const burgerMenuButtonIcon = document.querySelector('.burger-menu-button i')
+    const burgerMenu = document.querySelector('.burger-menu')
+
+    burgerMenuButton.onclick =function(){
+      burgerMenu.classList.toggle('open')
+        const isOpen = burgerMenu.classList.contains('open')
+        burgerMenuButtonIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+    }
--- BilletterieScript.js
+++ BilletterieScript.js
@@ -1,3 +1,5 @@
+/* Script bouton Login/inscription */
+
 const btnPopup = document.querySelector('.btnLogin-popup');
 const coverBox = document.querySelector('.cover-box');
 const loginLink = document.querySelector('.login-link');
@@ -26,4 +28,33 @@ loginLink.addEventListener('click', deactivateCoverBox);
 registerLink.addEventListener('click', activatePopup);
 iconClose.addEventListener('click', deactivateCoverBox);
 
+
+/* SCRIPT QR CODE */
+
+const container = document.querySelector(".container");
+qrInput = container.querySelector(".form input");
+generateBtn = container.querySelector(".form button");
+qrImg = container.querySelector(".qr-code img");
+
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        container.classList.remove("active");
+        preValue = "";
+    }
+});
 
